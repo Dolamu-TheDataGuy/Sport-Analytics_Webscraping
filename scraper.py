030265d7991786cb1d29ff522e4d75e443d54437
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-01-15 14:40:22 +0100

    added conditional statement for modular code

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 from TableScraper import Scraper
 
-table = Scraper()
-table.extract()
-print(table.dataframe(18))
-table.save(18)
+
+if __name__ == "__main__":
+    table = Scraper()
+    table.extract()
+    print(table.dataframe(18))
+    table.save(18)
