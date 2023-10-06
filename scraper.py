a586a113a33b742c50e3730af94229e8bd08063c
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-06 13:13:31 +0100

    ran code for gameweek 7 and extract dataframe and json file"

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from TableScraper import Scraper
 if __name__ == "__main__":
     table = Scraper()
     table.extract()
-    table.to_json(6)
-    print(table.dataframe(6))
-    table.save(6)
+    table.to_json(7)
+    print(table.dataframe(7))
+    table.save(7)
 
