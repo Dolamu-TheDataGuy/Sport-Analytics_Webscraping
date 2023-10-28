d59c2e94f9c537e3cb6807b7e31b15788faed68d
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-28 13:34:08 +0100

    ran code for gameweek 9 a bit late, one game is GW10 was added.

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from TableScraper import Scraper
 if __name__ == "__main__":
     table = Scraper()
     table.extract()
-    table.to_json(8)
-    print(table.dataframe(8))
-    table.save(8)
+    table.to_json(9)
+    print(table.dataframe(9))
+    table.save(9)
 
