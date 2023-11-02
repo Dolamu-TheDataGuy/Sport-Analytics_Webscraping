53ffe427192607c7ebe2d5f6504ca19a618f0598
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-11-02 21:35:48 +0100

    ran code for gameweek 10 and uploaded data

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from TableScraper import Scraper
 if __name__ == "__main__":
     table = Scraper()
     table.extract()
-    table.to_json(9)
-    print(table.dataframe(9))
-    table.save(9)
+    table.to_json(10)
+    print(table.dataframe(10))
+    table.save(10)
 
