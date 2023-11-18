290f6e589d79606e995245da03987f1941cc111a
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-11-19 00:45:57 +0100

    ran code for gameweek12 and also downloaded json and csv data.

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from TableScraper import Scraper
 if __name__ == "__main__":
     table = Scraper()
     table.extract()
-    table.to_json(11)
-    print(table.dataframe(11))
-    table.save(11)
+    table.to_json(12)
+    print(table.dataframe(12))
+    table.save(12)
 
